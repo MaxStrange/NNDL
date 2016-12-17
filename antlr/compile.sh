f2bf99c9c03284b5e3b1dfb1a40bf20421f76ee0
Max Strange <maxfieldstrange@gmail.com>
2016-12-16 22:43:04 -0800

    changed the compile script

--- antlr/compile.sh
+++ antlr/compile.sh
@@ -1,3 +1,4 @@
-antlr4 NNDL.g4
-javac -d ./antlrBuild -classpath /usr/local/lib/antlr-4.6-complete.jar ./antlrBuild/NNDL*.java
+#! /usr/bin/env bash
+java -jar /usr/local/lib/antlr-4.6-complete.jar NNDL.g4 -o ./antlrBuild
+javac -d ./antlrBuild -classpath /usr/local/lib/antlr-4.6-complete.jar -classpath ./antlrBuild ./antlrBuild/*.java
 
