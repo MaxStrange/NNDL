4c772e741f957eef5468444e6281e64d72b3dbe9
Max Strange <max.strange@synapse.com>
2017-01-06 18:23:04 -0800

    removed debugging stuff from main.cpp

--- simpractice/src/main.cpp
+++ simpractice/src/main.cpp
@@ -13,16 +13,7 @@
 
 #include SINK_HEADER
 #include SOURCE_HEADER
-#include BIAS_HEADER
-#include SYNAPSE_HEADER
 
-
-#if 0
-std::vector<Layer *> create_layers(void);
-std::vector<Synapse *>& connect_layers(const std::vector<Layer *> &layers,
-        std::vector<Synapse *> &connections);
-Signal generate_random_weight();
-#endif
 
 int main(int argv, char **argc)
 {
@@ -76,102 +67,6 @@ int main(int argv, char **argc)
         std::cout << "Simulator has been run!" << std::endl;
     #endif
 }
-
-#if 0
-std::vector<Layer *> create_layers(void)
-{
-    std::cout << "Creating the layers..." << std::endl;
-
-    std::vector<Layer *> layers;
-    Layer *input = new Layer();
-#if ! AND_EXAMPLE
-    Layer *hidden = new Layer();
-#endif
-    Layer *output = new Layer();
-    for (unsigned int i = 0; i < NUM_NEURONS_INPUT; i++)
-    {
-        Neuron n("in_" + std::to_string(i), true, false);
-        input->add_neuron(n);
-    }
-#if ! AND_EXAMPLE
-    for (unsigned int i = 0; i < NUM_NEURONS_HIDDEN; i++)
-    {
-        Neuron n("h_" + std::to_string(i), false, false);
-        hidden->add_neuron(n);
-    }
-#endif
-    for (unsigned int i = 0; i < NUM_NEURONS_OUTPUT; i++)
-    {
-        Neuron n("out_" + std::to_string(i), false, true);
-        output->add_neuron(n);
-    }
-
-    layers.push_back(input);
-#if ! AND_EXAMPLE
-    layers.push_back(hidden);
-#endif
-    layers.push_back(output);
-
-    return layers;
-}
-
-std::vector<Synapse *>& connect_layers(const std::vector<Layer *> &layers,
-        std::vector<Synapse *> &connections)
-{
-    std::cout << "Connecting layers..." << std::endl;
-
-    for (unsigned int i = 0; i < layers.size(); i++)
-    {
-        std::cout << "Connecting layer " << std::to_string(i) << std::endl;
-        std::cout << "Number of neurons in this layer: " <<
-                std::to_string(layers.at(i)->size()) << std::endl;
-        if (i != layers.size() - 1)
-        {
-            //We are not the last layer, so add forward connections
-            Layer *layer_i = layers.at(i);
-            for (unsigned int j = 0; j < layer_i->size(); j++)
-            {
-                Neuron *neuron_j_pointer = layer_i->at(j);
-                Layer *layer_next = layers.at(i + 1);
-                //Connect neuron_j to each neuron_l in layer i + 1
-                for (unsigned int l = 0; l < layer_next->size(); l++)
-                {
-                    Signal w = generate_random_weight();
-                    std::cout << "Weight (" << i << "_" << j << ", " << i + 1
-                            << "_" << l << "): " << w << std::endl;
-                    Neuron *neuron_l_pointer = layer_next->at(l);
-                    //Synapse from j to l
-                    Synapse *s = new SYNAPSE(neuron_j_pointer, neuron_l_pointer, w);
-                    if (j == 0)
-                    {
-                        std::cout << "Bias weight (" << i << "_" << j << ", " << i + 1
-                                << "_" << l << "): " << w << std::endl;
-
-                        Synapse *b = new BIAS(neuron_j_pointer, neuron_l_pointer, w);
-                        connections.push_back(b);
-                    }
-                    connections.push_back(s);
-                }
-            }
-        }
-    }
-    return connections;
-}
-
-Signal generate_random_weight()
-{
-    static auto seed = 5;
-    std::mt19937 rng(seed++);
-    double min = -0.5;
-    double max = 0.5;
-    std::uniform_real_distribution<double> uni(min, max);
-
-    double rn = uni(rng);
-
-    return Signal(rn);
-}
-
-#endif
 
 
 
